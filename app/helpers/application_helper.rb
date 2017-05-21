d248a0649cf604bddc85faa051bb1da23a01f330
giangbimin <gianghoang.dev@gmail.com>
2017-05-21 15:54:38 +0700

    Articles :feature 2 add color to code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,30 @@
 # ApplicationHelper
 module ApplicationHelper
+  # color code
+  class ColorCode < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+
   def markdown(content)
     opitions = {
-      filter_html: true,
       hard_wrap: true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
+      filter_html: true
     }
 
     extensions = {
+      space_after_headers: true,
+      fenced_code_blocks: true,
       autolink: true,
+      link_attributes: { rel: 'nofollow', target: "_blank" },
       no_intra_emphasis: true,
       disable_indented_code_blocks: true,
       lax_spacing: true,
       strikethrough: true,
-      superscript: true,
-      hard_wrap: true,
-      prettify: true
+      superscript: true
     }
-    renderer = Redcarpet::Render::HTML.new(opitions)
+    renderer = ColorCode.new(opitions)
     Redcarpet::Markdown.new(renderer, extensions).render(content).html_safe
   end
 end
